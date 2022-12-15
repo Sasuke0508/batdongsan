88cb7e702c45d494f06bfcf6d2d2d572aeee801e
sasuke <quana62000@gmail.com>
2022-12-16 00:56:52 +0700

    upload image firebase

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import ToastMessage from "./components/core/ToastMessage";
 import Footer from "./components/Footer";
 import Header from "./components/Header.js";
 import Pages from "./pages/index.js";
+
 function App() {
     const [loading, setLoading] = useState(false);
+
     return (
         <div className="App">
             {loading ? (
