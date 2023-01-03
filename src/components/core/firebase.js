d48718190cab376a4cb7a3d2865d3d1f34f60a88
sasuke <datdh@omegatheme.com>
2023-01-04 00:54:19 +0700

    switch fire base account

--- src/components/core/firebase.js
+++ src/components/core/firebase.js
@@ -7,14 +7,13 @@ import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyATyRHDTAaLvYVnnIS9KRSDhHFWzNdt7b8",
-    authDomain: "batdongsan-370317.firebaseapp.com",
-    projectId: "batdongsan-370317",
-    storageBucket: "batdongsan-370317.appspot.com",
-    messagingSenderId: "126021146171",
-    appId: "1:126021146171:web:0a59c0ec6a0c788a602e1e",
-    measurementId: "G-367TDBQ23E",
-    databaseURL: "https://batdongsan-370317-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    apiKey: "AIzaSyCOql3yZoOrjvFJi3dHml-4-c5mMjSn5Hg",
+    authDomain: "findroom-373109.firebaseapp.com",
+    projectId: "findroom-373109",
+    storageBucket: "findroom-373109.appspot.com",
+    messagingSenderId: "783269658664",
+    appId: "1:783269658664:web:25c55277eff406c6e0f51d",
+    measurementId: "G-NDEECMC1W8",
 };
 
 // Initialize Firebase
